66721f8d9fa65aee73d9d6f4a6a40fc22ded9f56
flyinggorilla <flyinggorilla@outlook.com>
2020-02-23 12:07:15 +0100

    updated materials to work with materialstation

---
+++ deletematerials.cmd
@@ -0,0 +1,6 @@
+python ums5material.py -l -F ApolloX
+python ums5material.py -d FormFutura_ApolloX_ASA_Natural.xml.fdm_material
+python ums5material.py -d FormFutura_ApolloX_ASA_White.xml.fdm_material
+python ums5material.py -d FormFutura_ApolloX_ASA_Red.xml.fdm_material
+python ums5material.py -d FormFutura_ApolloX_ASA_Black.xml.fdm_material
+python ums5material.py -l -F ApolloX
