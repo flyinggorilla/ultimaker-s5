9c6a70a588c3851c9fa9444f070500b6d1ced684
flyinggorilla <flyinggorilla@outlook.com>
2020-03-12 22:17:13 +0100

    updated and tested with Cura 4.5, 5.5.12 firmware

--- updatematerials.cmd
+++ updatematerials.cmd
@@ -1,6 +1,16 @@
-python ums5material.py -l -F ApolloX
+python ums5material.py -l -F FormFutura
 python ums5material.py -u FormFutura_ApolloX_ASA_Natural.xml.fdm_material
 python ums5material.py -u FormFutura_ApolloX_ASA_White.xml.fdm_material
 python ums5material.py -u FormFutura_ApolloX_ASA_Red.xml.fdm_material
 python ums5material.py -u FormFutura_ApolloX_ASA_Black.xml.fdm_material
-python ums5material.py -l -F ApolloX
+python ums5material.py -u FormFutura_HDglass_PETG_Clear.xml.fdm_material
+python ums5material.py -u FormFutura_HDglass_PETG_SeeThroughBlack.xml.fdm_material
+python ums5material.py -u FormFutura_HDglass_PETG_SeeThroughBlue.xml.fdm_material
+python ums5material.py -u FormFutura_HDglass_PETG_SeeThroughRed.xml.fdm_material
+python ums5material.py -u FormFutura_Helios_PVA.xml.fdm_material
+python ums5material.py -u FormFutura_Atlas_PVA.xml.fdm_material
+python ums5material.py -u FormFutura_CrystalFlex_Clear.xml.fdm_material
+python ums5material.py -u FormFutura_PythonFlex_Black.xml.fdm_material
+python ums5material.py -u FormFutura_PythonFlex_Clear.xml.fdm_material
+python ums5material.py -u FormFutura_PythonFlex_White.xml.fdm_material
+python ums5material.py -l -F FormFutura
